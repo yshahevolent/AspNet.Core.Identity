e8e27ea7e304adfe06abcb3c90c37f61fe105830
Yash Shah <yshah@evolenthealth.com>
2022-08-19 20:24:50 +0530

    changes

---
+++ IdentityExample/Startup.cs
@@ -0,0 +1,79 @@
+using IdentityExample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(config => config.UseInMemoryDatabase("Memory"));
+
+            // AddIdentity registers the services 
+            services.AddIdentity<IdentityUser, IdentityRole>(config => 
+            {
+                config.Password.RequireDigit = false;
+                config.Password.RequiredLength = 2;
+                config.Password.RequireUppercase = false;
+                config.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.LoginPath = "/Home/Login";
+            });
+
+            //services.AddAuthentication("CookieAuth")
+            //    .AddCookie("CookieAuth", config =>
+            //    {
+            //        config.Cookie.Name = "Basic.Cookie";
+            //        config.LoginPath = "/Home/Authenticate";
+            //    });
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+
+            //Who are you?
+            app.UseAuthentication();
+
+            //Are you allowed?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
